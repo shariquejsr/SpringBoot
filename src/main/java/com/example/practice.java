f4129eee25646f463c625dd778eb21adee7d9d8d
shariquejsr <shariquerafique30@gmail.com>
2025-10-06 12:48:07 +0530

    Added new file

---
+++ src/main/java/com/example/practice.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api")
+public class practice {
+
+
+
+}
